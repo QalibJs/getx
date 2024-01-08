2426e76a7007c8e048b9fab358fa7bc03dda7c36
Qalib Alizade <qalibalizade@Qalibs-MacBook-Air.local>
2024-01-08 18:31:05 +0400

    third commit

--- lib/extension/extension.dart
+++ lib/extension/extension.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
 extension Sizes on BuildContext {
-  double get witdh => MediaQuery.of(this).size.width;
-  double get height => MediaQuery.of(this).size.height;
+  Size get witdh => MediaQuery.of(this).size;
+  Size get height => MediaQuery.of(this).size;
 }
